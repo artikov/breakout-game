794afdcdd420999a8140dfdceb5dbd2eab310135
artikov <oybekartikov@icloud.com>
2022-09-18 14:42:23 +0500

    ball class created, minor improvements

--- main.py
+++ main.py
@@ -1,15 +1,53 @@
 import tkinter as tk
 from turtle import Screen
+from ball import Ball
 from paddle import Paddle
+import time
 
 # game screen initialized
 screen = Screen()
 screen.bgcolor('black')
 screen.setup(width=600, height=600)
 screen.title('Breakout')
+screen.tracer(0)
 
 # initializing paddle object
 paddle = Paddle()
+
+# initializing ball object
+ball = Ball()
+
+screen.listen()
+screen.onkey(paddle.go_left, 'Left')
+screen.onkey(paddle.go_right, 'Right')
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+    # detecting collision with side walls
+    if ball.xcor() > 280 or ball.xcor() < -280:
+        ball.bounce_x()
+
+    # TEMP check if ball collided with top wall
+    if ball.ycor() > 150: # can use as wall start position
+        ball.bounce_y()
+
+    # detect collision with paddle
+    if ball.distance(paddle) < 50 and ball.ycor() < -240: # 320
+        print('made contact')
+        ball.bounce_y()
+
+    # game winning and loosing conditions
+    if ball.ycor() > 300:
+        print('player won')
+        break
+    if ball.ycor() < -290:
+        print('you loose')
+        ball.reset_position()
+        # break
 
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -10,3 +10,11 @@ class Paddle(Turtle):
         self.shapesize(stretch_len=5, stretch_wid=1)
         self.penup()
         self.goto(0, -270)
+
+    def go_left(self):
+        new_x = self.xcor() - 20
+        self.goto(new_x, self.ycor())
+
+    def go_right(self):
+        new_x = self.xcor() + 20
+        self.goto(new_x, self.ycor())
